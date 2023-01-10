6b5f2d0bb9f9ec76eb9db46603fd7bd09f7c01ca
Lucas Tabone <88245279+LucasTabone@users.noreply.github.com>
2023-01-09 21:32:37 -0300

    ajuste - 003

--- desafio.kt
+++ desafio.kt
@@ -1,5 +1,6 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
+
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
 data class Usuario ( val nome: String , val idade: Int , val: nota )

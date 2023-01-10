8636c67b7b1ea90f52d6d243bbb0f9ea508b237a
Lucas Tabone <88245279+LucasTabone@users.noreply.github.com>
2023-01-09 21:33:36 -0300

    ajustes - 004

--- desafio.kt
+++ desafio.kt
@@ -1,9 +1,8 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-data class Usuario ( val nome: String , val idade: Int , val: nota )
+class Usuario (val nome: String, val idade: Int, val id: id)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
@@ -12,11 +11,21 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
+    fun nota: Int {
+        val nota {
+          if ( nota => 5) {
+            println(aprovado)
+        } else println(reprovado)
+    }
+        }
+    
 }
 
 fun main() {
+    
+    val lucas = Usuario ( Lucas, 2 , )
     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
